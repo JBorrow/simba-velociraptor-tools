3f01906acc0996b16a4f71b1ebd92efc07291ce9
Josh Borrow <joshua.borrow@durham.ac.uk>
2018-11-07 11:22:40 +0000

    Minor fix with incorrect filename

--- preprocess.py
+++ preprocess.py
@@ -101,9 +101,9 @@ def load_hdf5_replace_and_dump(filename: str, output_filename_extra="duplicated"
     and writes to the original HDF5 file.
     """
 
-    existing_particle_types, id_array_list = zip(*read_particle_ids_from_file(
-        f"{filename}.hdf5"
-    ).items())
+    existing_particle_types, id_array_list = zip(
+        *read_particle_ids_from_file(f"{filename}.hdf5").items()
+    )
     id_array, insertion_points = combine_arrays(id_array_list)
 
     new_id_array, old_position_dict, new_position_dict = find_and_replace_non_unique_ids(
@@ -115,7 +115,7 @@ def load_hdf5_replace_and_dump(filename: str, output_filename_extra="duplicated"
     duplicated_filename = f"{filename}_{output_filename_extra}.yml"
     write_data(duplicated_filename, old_position_dict, new_position_dict)
 
-    write_all_id_arrays(filename, new_id_array_list, existing_particle_types)
+    write_all_id_arrays(f"{filename}.hdf5", new_id_array_list, existing_particle_types)
 
     return
 
